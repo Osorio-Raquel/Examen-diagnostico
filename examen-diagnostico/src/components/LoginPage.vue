bfa627c0aed64455f1af56f04a7528b73f6d6b59
Raquel Osorio <142281150+Osorio-Raquel@users.noreply.github.com>
2025-02-04 14:48:17 -0400

    vue con vuetify levantado

---
+++ examen-diagnostico/src/components/LoginPage.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+<h1>HELLO WORLD</h1>
+</template>
+
+<style  scoped>
+.container{
+    background: #f43535;
+}
+
+h1{
+    background-color: aquamarine;
+    color: black;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+</style>
